714375d7b3abd24f35c7be80b5bfce60ce3c3011
Wihelmina_Waromi <>
2022-03-19 07:20:54 +0700

    Done

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    //Loop
+
+    //while Loop
+    //Do while Loop
+    //for Loop
+    //Foreach Loop
+    $array = array("Daniel", "Jane", "Jacob", "Mariane");
+
+    foreach ($array as $loopdata) {
+        echo "My name is ".$loopdata."<br>"
+    }
